1fa529a8008da3984c7703324063c245d22db82a
Matthew Fernandez <matthew.fernandez@nicta.com.au>
2014-11-27 14:46:26 +1100

    Template context: Expose `chr` and `ord`.
    Needed in order to work around some idiocy in Isabelle.
    
    Conflicts:
    	camkes/runner/Context.py

--- camkes/runner/Context.py
+++ camkes/runner/Context.py
@@ -155,6 +155,9 @@ def new_context(entity, configuration, obj_space, cap_space, shmem, **kwargs):
         'set':DeterministicSet,
         'str':str,
         'splitext':os.path.splitext,
+        'arch':os.environ.get('ARCH', ''),
+        'ord':ord,
+        'chr':chr,
 
         # Allocation pools. In general, do not touch these in templates, but
         # interact with them through the alloc* functions. They are only in the
