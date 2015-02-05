1b8e180e35b7a8b3c0e0ac95d1ef4c326b0e37da
Matthew Fernandez <matthew.fernandez@nicta.com.au>
2015-02-05 15:15:32 +1100

    Add some missing core libraries.
    These were already implicit dependencies.

--- camkes.mk
+++ camkes.mk
@@ -111,7 +111,7 @@ endif
 # come before sel4platsupport to make sure we get the sel4camkes _start, not
 # the sel4platsupport one.
 CAMKES_CORE_LIBS = sel4 sel4debug c ${CONFIG_CAMKES_SYSLIB} sel4camkes \
-    sel4sync utils sel4vka sel4utils
+    sel4sync utils sel4vka sel4utils sel4platsupport platsupport
 
 PRUNER_BLACKLIST = FILE fpos_t opterr optind optopt stderr stdin stdout va_list __isoc_va_list max_align_t
 
