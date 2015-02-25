e6da2a3cb5bc8a9e32eecb55bd994de2a1b28066
Stephen Sherratt <stephen.sherratt@nicta.com.au>
2015-02-25 17:23:06 +1100

    made assign_address_spaces a POST_RESOLUTION AST_TRANSFORM and combining assemblies after resolving imports

--- camkes/runner/__main__.py
+++ camkes/runner/__main__.py
@@ -151,7 +151,6 @@ def main():
                     done(value.output)
         with profiler('Parsing input'):
             ast = parser.parse_to_ast(s, options.cpp, options.cpp_flag, options.ply_optimise)
-            ast = compose_assemblies(ast)
             parser.assign_filenames(ast, f.name)
     except parser.CAmkESSyntaxError as e:
         e.set_column(s)
@@ -173,6 +172,10 @@ def main():
                 options.cpp, options.cpp_flag, options.ply_optimise)
     except Exception as inst:
         die('While resolving imports of \'%s\': %s' % (f.name, str(inst)))
+
+    # if there are multiple assemblies, combine them now
+    ast = compose_assemblies(ast)
+
     with profiler('Caching original AST'):
         orig_ast = deepcopy(ast)
     with profiler('Deduping AST'):
