b1caff51a29c76e6fd53bc1c57416786143c3452
Stephen Sherratt <stephen.sherratt@nicta.com.au>
2015-04-15 14:23:17 +1000

    Only type-checking hierarchical attributes when the referent and referer are user-defined.

--- camkes/runner/__main__.py
+++ camkes/runner/__main__.py
@@ -636,13 +636,20 @@ def merge_assembly(dest, source, instance):
                                 "by nested component instance %s"
                                 % (s.value, instance.name))
 
-            # Check that the types of the referer and referent are the same
-            referer_type = nested_instance_attributes[s.instance][s.attribute].type
-            referent_type = instance_attributes[reference].type
-            if referer_type != referent_type:
-                raise Exception("Attribute type mismatch: attribute %s (%s) refers to "
-                                "attribute %s (%s)."
-                                % (s.attribute, referer_type, reference, referent_type))
+            if s.instance in nested_instance_attributes and \
+                s.atttribute in nested_instance_attributes[s.instance] and \
+                reference in instance_attributes:
+
+                # the referer and referent are both user-defined types, so they
+                # can be type checked
+
+                # Check that the types of the referer and referent are the same
+                referer_type = nested_instance_attributes[s.instance][s.attribute].type
+                referent_type = instance_attributes[reference].type
+                if referer_type != referent_type:
+                    raise Exception("Attribute type mismatch: attribute %s (%s) refers to "
+                                    "attribute %s (%s)."
+                                    % (s.attribute, referer_type, reference, referent_type))
 
             s.value = dest.configuration[instance.name][reference]
 
