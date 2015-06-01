85397a8d00b77bb5d4b6a52bd44daed60f981bea
Matthew Fernandez <matthew.fernandez@nicta.com.au>
2015-06-01 17:07:56 +1000

    label-mapping: Automate a proof of valid_extra.

--- camkes/templates/label-mapping.thy
+++ camkes/templates/label-mapping.thy
@@ -10,8 +10,9 @@
 
 /*- set thy = os.path.splitext(os.path.basename(options.outfile.name))[0] -*/
 theory /*? thy ?*/ imports
-  "../../spec/capDL/CapDLSpec"
+  "../../spec/capDL/CapDLSpec" (* generated *)
   Generator_CAMKES_CDL
+  "../adl-spec/ArchSpec" (* generated *)
 begin
 
 /*# Ignore the comment below. It is intended to apply to the generated output,
@@ -128,9 +129,81 @@ definition extra' :: cdl_heap
     None"
 (** TPP: condense = False *)
 
+(** TPP: condense = True *)
+lemma extra_only_pds_pts_frames:
+  "case extra' x of None \<Rightarrow> True
+                  | Some (PageDirectory _) \<Rightarrow> True
+                  | Some (PageTable _) \<Rightarrow> True
+                  | Some (Frame _) \<Rightarrow> True
+                  | _ \<Rightarrow> False"
+  by (simp add:extra'_def,
+  /*- for obj in obj_space.spec.objs -*/
+    /*- if obj.name is not none and isinstance(obj, (capdl.Frame, capdl.PageTable, capdl.PageDirectory)) -*/
+      case_tac "x = /*? obj.name ?*/_id",
+       simp add:/*? obj.name ?*/_def,
+    /*- endif -*/
+  /*- endfor -*/
+      simp)
+(** TPP: condense = False *)
+
+lemma union_split: "\<lbrakk>A \<inter> C = {}; B \<inter> C = {}\<rbrakk> \<Longrightarrow> (A \<union> B) \<inter> C = {}"
+by (simp add: inf_sup_distrib2)
+
 interpretation Generator_CAMKES_CDL.cdl_translation ipc_buffer id_of
   apply unfold_locales
    apply (simp add:buffers_distinct')
   by (simp add:ids_distinct')
+
+lemma cnode_objects_distinct:
+  "dom (map_of (map (\<lambda>(x, y). (the (id_of x), y))
+     (cnode_objs ArchSpec.assembly'))) \<inter> dom extra' = {}"
+  by (subst dom_map_of_conv_image_fst,
+      simp add:cnode_objs_def ArchSpec.assembly'_def ArchSpec.composition'_def
+               id_of_def instance_names_def dom_def,
+      code_simp)
+
+(** TPP: condense = True *)
+/*- set to_unfold = set(('tcb_objs_def', 'ArchSpec.assembly\'_def', 'ArchSpec.composition\'_def', 'id_of_def', 'instance_names_def', 'dom_def')) -*/
+/*# During the proof of this lemma we'll need to unfold the definitions of all
+ *# the component instances and their types.
+ #*/
+/*- for i in composition.instances -*/
+  /*- do map(to_unfold.add, ('ArchSpec.%s_def' % i.name, 'ArchSpec.%s_def' % i.type.name)) -*/
+/*- endfor -*/
+lemma tcb_objects_distinct:
+  "dom (map_of (map (\<lambda>(x, y). (the (id_of x), y))
+     (tcb_objs ArchSpec.assembly'))) \<inter> dom extra' = {}"
+  by (subst dom_map_of_conv_image_fst,
+/*? '\n'.join(textwrap.wrap(' '.join(to_unfold), width=100, initial_indent='      simp add:', subsequent_indent=' ' * len('      simp add:'))) ?*/,
+      code_simp)
+(** TPP: condense = False *)
+
+(** TPP: condense = True *)
+/*- set to_unfold = set(('ArchSpec.assembly\'_def', 'ArchSpec.composition\'_def', 'ep_objs_def', 'ep_objs\'_def', 'id_of_def', 'dom_def')) -*/
+/*# During the proof of this lemma we'll need to unfold the definitions of all
+ *# the connections.
+ #*/
+/*- for c in composition.connections -*/
+  /*- do to_unfold.add('ArchSpec.%s_def' % c.name) -*/
+/*- endfor -*/
+lemma ep_objects_distinct:
+  "dom (map_of (map (\<lambda>(x, y). (the (id_of x), y))
+     (ep_objs ArchSpec.assembly'))) \<inter> dom extra' = {}"
+  apply (subst dom_map_of_conv_image_fst)
+/*? '\n'.join(textwrap.wrap(' '.join(to_unfold), width=100, initial_indent='  apply (simp add:', subsequent_indent=' ' * len('  apply (simp add:'))) ?*/)
+  (* XXX: Connection name mangling prevents the following proof for now. *)
+  sorry
+(** TPP: condense = False *)
+
+lemma objects_distinct: "dom (cdl_objects (generate' ArchSpec.assembly')) \<inter> dom extra' = {}"
+  apply (simp add:generate'_def obj_heap_def)
+  apply (rule union_split)
+   apply (simp add:cnode_objects_distinct)
+  apply (rule union_split)
+   apply (simp add:tcb_objects_distinct)
+  by (simp add:ep_objects_distinct)
+
+lemma extra_is_valid: "valid_extra (generate' ArchSpec.assembly') extra'"
+  by (simp add:valid_extra_def extra_only_pds_pts_frames objects_distinct)
 
 end
