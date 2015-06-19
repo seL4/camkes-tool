ecf1e00d4148373bd611968469ee499e8b1169b5
Matthew Fernandez <matthew.fernandez@nicta.com.au>
2015-06-19 14:07:24 +1000

    Trivial: Optimise a loop-continue into a generator expression.

--- camkes/runner/Filters.py
+++ camkes/runner/Filters.py
@@ -451,9 +451,8 @@ def replace_dma_frames(ast, obj_space, elfs, options, **_):
 
     assembly = find_assembly(ast)
 
-    for i in assembly.composition.instances:
-        if i.type.hardware:
-            continue
+    for i in (x for x in assembly.composition.instances
+            if not x.type.hardware):
 
         perspective = Perspective(instance=i.name, group=i.address_space)
 
