0fdc502e2a0d9e4262c532c17acc72704dd561c9
Stephen Sherratt <Stephen.Sherratt@nicta.com.au>
2015-10-15 14:24:47 +1100

    Added libsel4vspace to the list of core libraries that all camkes binaries should be linked against.

--- camkes.mk
+++ camkes.mk
@@ -113,7 +113,7 @@ endif
 # come before sel4platsupport to make sure we get the sel4camkes _start, not
 # the sel4platsupport one.
 CAMKES_CORE_LIBS = sel4 sel4debug c $(patsubst "%",%,${CONFIG_CAMKES_SYSLIB}) sel4camkes \
-    sel4sync utils sel4vka sel4utils sel4platsupport platsupport
+    sel4sync utils sel4vka sel4utils sel4platsupport platsupport sel4vspace
 
 PRUNER_BLACKLIST = FILE fpos_t opterr optind optopt stderr stdin stdout \
   va_list __isoc_va_list max_align_t camkes_error_t camkes_error_handler_t \
