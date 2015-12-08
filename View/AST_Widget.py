73e9982d7b869073a29442b97bec3630853fed77
Shanush Prema Thasarathan <ShanushPrema.Thasarathan@nicta.com.au>
2015-12-08 18:43:05 +1100

    Created an AST_Widget. Useless atm

--- View/AST_Widget.py
+++ View/AST_Widget.py
@@ -6,8 +6,21 @@ import cairo
 
 from camkes.ast.base import ASTObject
 
+from Controller.base_controller import Controller
 
-class ASTWidget(Gtk.DrawingArea):
-    def __init__(self, ast_object):
+
+class ASTWidget(Gtk.Box):
+
+    @property
+    def widget_controller(self):
+        return self._widget_controller
+
+    @widget_controller.setter
+    def widget_controller(self, value):
+        assert issubclass(value.__class__, Controller) or isinstance(value, Controller)
+
+    def __init__(self, ast_object, widget_controller):
         super(ASTWidget, self).__init__()
-        self.add_child(Gtk.Label("Hello"))
+        self._widget_Controller = widget_controller
+
+        self.add()
