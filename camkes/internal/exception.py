7bc071baaae0a0e9b2625d2b27de8cb405e766a0
Matthew Fernandez <matthew.fernandez@nicta.com.au>
2016-01-15 19:32:55 +1100

    Add a generic parent for CAmkES errors.
    This has no function yet, but in upcoming commits this will become a
    repository for generic error functionality.

---
+++ camkes/internal/exception.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+# Copyright 2016, NICTA
+#
+# This software may be distributed and modified according to the terms of
+# the BSD 2-Clause license. Note that NO WARRANTY is provided.
+# See "LICENSE_BSD2.txt" for details.
+#
+# @TAG(NICTA_BSD)
+#
+
+from __future__ import absolute_import, division, print_function, \
+    unicode_literals
+from camkes.internal.seven import cmp, filter, map, zip
+
+class CAmkESError(Exception):
+    '''
+    Generic basis for CAmkES errors.
+    '''
+    pass
