f375b47eb8e9d35e959095cf7bc79fe7be433e13
Shanush Prema Thasarathan <shanushprema.thasarathan@nicta.com.au>
2016-02-15 08:05:24 +1100

    Added window icon

--- __main__.py
+++ __main__.py
@@ -6,7 +6,7 @@ import sys, os
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../../'))
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtGui
 
 from Controller.graph_controller import GraphController
 
@@ -24,8 +24,9 @@ from Controller.graph_controller import GraphController
 
 def main(argv, out, err):
     app = QtWidgets.QApplication(argv)
-
-    # new_controller = GraphController("/home/sthasarathan/Documents/camkes-newExample/apps/complex/complex.camkes")
+    app.setApplicationName("VisualCAmkES")
+    app.setWindowIcon(QtGui.QIcon(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'Assests/VisualCAmkES.png')))
+    # new_controller = graphcontroller("/home/sthasarathan/documents/camkes-newexample/apps/complex/complex.camkes")
     new_controller = GraphController()
 
     # new_controller = GraphController("/home/sthasarathan/Documents/CAMKES-APPS/camkes-kitty-HDDMA/apps/bilbyfs/bilbyfs.camkes")
