cdba91e3ef8d0ad099ff49604d6157f32046fe48
Shanush <shanushau@yahoo.com.au>
2016-03-14 10:35:10 +1100

    Started documentation inside __init__

---
+++ camkes/visualCAmkES/Controller/__init__.py
@@ -0,0 +1,3 @@
+# The Controller python module.
+
+# This module contains graph_controller, which is the main window that the user sees. GraphController deals with the getting a file specified by the user, high level event handling (like File, View menus), and creating an AST from the model (ASTModel) to give to View(GraphWidget)
---
+++ camkes/visualCAmkES/__init__.py
@@ -0,0 +1,4 @@
+# VisualCAmkES module
+# This file exist because visualCAmkES is a module.
+
+# To use the program, python visualCAmkES
