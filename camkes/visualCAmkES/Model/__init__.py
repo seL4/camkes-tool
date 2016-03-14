2c60458a551c060d79cc8e6c4e1be7473abf252f
Shanush Prema Thasarathan <ShanushPrema.Thasarathan@nicta.com.au>
2016-03-14 11:16:41 +1100

    Wrote comments inside __init__ files, explaining why it exist and brief explanation of the module.

---
+++ camkes/visualCAmkES/Model/__init__.py
@@ -0,0 +1,8 @@
+# The Model python module
+
+# This module deals with model side of the program. It communicates with the parser to collect an AST tree (AST_Model.py
+# In the future ASTModel will create a unfrozen ast, allowing for editing.
+
+# Common contains all the simple helper functions used throughout the visualCAmkES module.
+
+# A lot of the model work is actually done in other camkes modules (such as parser, ast, runner, etc.)
--- camkes/visualCAmkES/__init__.py
+++ camkes/visualCAmkES/__init__.py
@@ -2,3 +2,6 @@
 # This file exist because visualCAmkES is a module.
 
 # To use the program, python visualCAmkES
+
+# The main function starts in __main__, invokes graphcontroller.
+# graphcontroller does all the work.
