6e3fecfd50ec49eee68ba8d6dc7c13b8bbf9865c
Shanush Prema Thasarathan <ShanushPrema.Thasarathan@nicta.com.au>
2016-04-01 11:27:22 +1100

    Border for hardware

--- camkes/visualCAmkES/View/Instance_Widget.py
+++ camkes/visualCAmkES/View/Instance_Widget.py
@@ -434,13 +434,21 @@ class InstanceWidget(QtWidgets.QGraphicsWidget):
             outline_rect = outline_rect.adjusted(-1,-1,1,1)
             rounded_rect = QtGui.QPainterPath()
             rounded_rect.addRoundedRect(outline_rect, 5,5)
-            
-            # Change color to BLUE
+
+            # Make a BLUE color pen
             pen = QtGui.QPen(QtCore.Qt.blue)
             pen.setWidth(5)
             painter.strokePath(rounded_rect, pen)
 
         # Draw outline to highlight hardware components
+        if self.hardware:
+            outline_rect = outline_rect.adjusted(-1,-1,1,1)
+            rounded_rect = QtGui.QPainterPath()
+            rounded_rect.addRoundedRect(outline_rect, 5,5)
+
+            pen = QtGui.QPen(QtCore.Qt.green)
+            pen.setWidth(5)
+            painter.strokePath(rounded_rect,pen)
 
 
         # TODO IDEA: Update rect with new size
