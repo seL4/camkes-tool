ec518c839183647f7e8d1dd6ae28de0f5ec62936
Matthew Fernandez <matthew.fernandez@nicta.com.au>
2016-05-19 16:09:20 +1000

    libsel4muslccamkes: Fix misspelling of syscall.
    This commit fixes a typo that led to `mmap2` being unknown and, as a result,
    calls to `malloc` that required mapping more memory would fail.
    
    Closes JIRA CAMKES-484

--- libsel4muslccamkes/src/vsyscall.c
+++ libsel4muslccamkes/src/vsyscall.c
@@ -45,7 +45,7 @@ static long (*syscall_table[])(va_list) = {
 #ifdef __NR_mmap
     [__NR_mmap] = sys_mmap,
 #endif
-#ifdef __NR__mmap2
+#ifdef __NR_mmap2
     [__NR_mmap2] = sys_mmap2,
 #endif
     [__NR_mremap] = sys_mremap,
