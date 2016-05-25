c728a09d60685412b44a34c7c39a8a06283508de
Matthew Fernandez <matthew.fernandez@nicta.com.au>
2016-05-25 10:56:36 +1000

    Add a test exposing two distinct bugs in the CPP dependency parser.
    This commit cherry-picks 7cdb8f6a170ce4574f16cae024ce8615970bdda4 from
    ssh://github.inside.nicta.com.au/mfernandez/project.
    
    JIRA CAMKES-450

--- camkes/parser/tests/testcpp.py
+++ camkes/parser/tests/testcpp.py
@@ -22,7 +22,7 @@ ME = os.path.abspath(__file__)
 sys.path.append(os.path.join(os.path.dirname(ME), '../../..'))
 
 from camkes.internal.tests.utils import CAmkESTest, cpp_available
-from camkes.parser.stage0 import CPP
+from camkes.parser.stage0 import CPP, parse_makefile_rule
 
 class TestCPP(CAmkESTest):
     def setUp(self):
@@ -147,6 +147,91 @@ class TestCPP(CAmkESTest):
         content, _ = parser.parse_string('HELLO')
 
         self.assertIn('world', content)
+
+    # The following tests probe the behaviour of the parse_makefile_rule
+    # function which has been buggy in the past.
+
+    def test_no_cpp_rules(self):
+        '''
+        Test that an empty list of Makefile rules yields no dependencies.
+        '''
+        filename = self.mkstemp()
+
+        # Create the file with no content.
+        with open(filename, 'wt') as f:
+            pass
+
+        # Confirm that we get no dependencies out of it.
+        with open(filename) as f:
+            deps = list(parse_makefile_rule(f))
+        self.assertLen(deps, 0)
+
+    def test_basic_cpp_rule(self):
+        '''
+        Test that we can correctly handle a basic Makefile rule.
+        '''
+        filename = self.mkstemp()
+
+        # Create a basic rule.
+        with open(filename, 'wt') as f:
+            f.write('target: a b\n')
+
+        # Confirm we get the correct dependencies.
+        with open(filename) as f:
+            deps = set(parse_makefile_rule(f))
+        self.assertSetEqual(deps, set(['a', 'b']))
+
+    def test_split_cpp_rule(self):
+        '''
+        Test that we can correctly handle rules split over multiple lines.
+        '''
+        filename = self.mkstemp()
+
+        # Create a split rule.
+        with open(filename, 'wt') as f:
+            f.write('target: a b \\\n'
+                    'c \\\n'
+                    'd\n')
+
+        # Confirm we get the correct dependencies.
+        with open(filename) as f:
+            deps = set(parse_makefile_rule(f))
+        self.assertSetEqual(deps, set(['a', 'b', 'c', 'd']))
+
+    def test_split_cpp_rule2(self):
+        '''
+        Test a more complicated split rule.
+        '''
+        filename = self.mkstemp()
+
+        # Create a complicated split rule.
+        with open(filename, 'wt') as f:
+            f.write('target: \\\n'
+                    'a b \\\n'
+                    '\\\n'
+                    'c\n')
+
+        # Confirm we get the correct dependencies.
+        with open(filename) as f:
+            deps = set(parse_makefile_rule(f))
+        self.assertSetEqual(deps, set(['a', 'b', 'c']))
+
+    def test_multiple_cpp_rules(self):
+        '''
+        We should ignore everything after the first Makefile dependency rule.
+        Test that this is actually true.
+        '''
+        filename = self.mkstemp()
+
+        # Create a file with multiple rules.
+        with open(filename, 'wt') as f:
+            f.write('target: a b c\n'
+                    'target2: d e f\n')
+
+        # Confirm we only get the dependencies of the first rule.
+        with open(filename) as f:
+            deps = set(parse_makefile_rule(f))
+        self.assertSetEqual(deps, set(['a', 'b', 'c']))
 
 if __name__ == '__main__':
     unittest.main()
