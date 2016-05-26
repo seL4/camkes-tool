233f7e5c51c6ebcc1c663beb79ceea5e7bdd0542
Stephen Sherratt <Stephen.Sherratt@nicta.com.au>
2016-05-26 16:19:31 +1000

    Removed non-existent CNodeSaveTCBCaller
    This syscall is only available on the rt kernel, and isn't used by any
    camkes templates. It was added to the kernel-agnostic abstraction layer
    by mistake. This change removes it.

--- libsel4camkes/include/camkes/sel4.h
+++ libsel4camkes/include/camkes/sel4.h
@@ -20,10 +20,8 @@
 enum camkes_invocation_label {
 #ifdef CONFIG_KERNEL_RT
     CamkesCNodeSaveCaller = CNodeSwapCaller,
-    CamkesCNodeSaveTCBCaller = CNodeSwapTCBCaller,
 #else
     CamkesCNodeSaveCaller = CNodeSaveCaller,
-    CamkesCNodeSaveTCBCaller = CNodeSaveTCBCaller,
 #endif
 };
 
@@ -33,15 +31,6 @@ static inline int camkes_cnode_save_caller(seL4_CNode service,
     return seL4_CNode_SwapCaller(service, index, depth);
 #else
     return seL4_CNode_SaveCaller(service, index, depth);
-#endif
-}
-
-static inline int camkes_cnode_save_tcb_caller(seL4_CNode service,
-    seL4_Word index, seL4_Uint8 depth, seL4_CPtr tcb) {
-#ifdef CONFIG_KERNEL_RT
-    return seL4_CNode_SwapTCBCaller(service, index, depth, tcb);
-#else
-    return seL4_CNode_SaveTCBCaller(service, index, depth, tcb);
 #endif
 }
 
