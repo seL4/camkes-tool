72b8df055f9d44265b180dc8234f0b374e975d6e
Matthew Fernandez <matthew.fernandez@nicta.com.au>
2016-06-01 11:07:00 +1000

    Add a test for unused macros.
    This commit cherry-picks 05cfffd1506e4dc28d831c2b339b3039270af6be from
    ssh://github.inside.nicta.com.au/mfernandez/project.

--- camkes/templates/tests/testmacros.py
+++ camkes/templates/tests/testmacros.py
@@ -18,16 +18,17 @@ This file contains unit test cases related to the template macros.
 from __future__ import absolute_import, division, print_function, \
     unicode_literals
 
-import os, re, subprocess, sys, unittest
+import ast, fnmatch, os, re, subprocess, sys, unittest
 
 ME = os.path.abspath(__file__)
+MY_DIR = os.path.dirname(ME)
 
 # Make CapDL importable. Note that we just assume where it is in relation to
 # our own directory.
-sys.path.append(os.path.join(os.path.dirname(ME), '../../../../python-capdl'))
+sys.path.append(os.path.join(MY_DIR, '../../../../python-capdl'))
 
 # Make CAmkES importable
-sys.path.append(os.path.join(os.path.dirname(ME), '../../..'))
+sys.path.append(os.path.join(MY_DIR, '../../..'))
 
 from camkes.internal.tests.utils import CAmkESTest, which
 from camkes.templates.macros import sizeof
@@ -61,6 +62,72 @@ class TestMacros(CAmkESTest):
         sz = sizeof('ia32', 'long')
 
         self.assertEqual(sz, 4)
+
+    def test_find_unused_macros(self):
+        '''
+        Find macros intended for the templates that are never actually used in
+        any template.
+        '''
+
+        # First obtain a set of available macros by parsing the source of
+        # macros.py.
+        macrospy = os.path.join(MY_DIR, '../macros.py')
+        with open(macrospy) as f:
+            source = f.read()
+
+        node = ast.parse(source, filename=macrospy)
+
+        macros = set()
+        for i in ast.iter_child_nodes(node):
+            if isinstance(i, ast.FunctionDef):
+                macros.add(i.name)
+
+        # Next get a set of gitignored globs.
+
+        # First up, ignore the tests.
+        ignored = set(('%s/**' % MY_DIR,))
+
+        # Now look at all .gitignores from three directories up.
+        for stem in ('../../..', '../..', '..'):
+            gitignore = os.path.join(MY_DIR, stem, '.gitignore')
+            if os.path.exists(gitignore):
+                with open(gitignore) as f:
+                    for line in (x.strip() for x in f
+                            if x.strip() != '' and not x.startswith('#')):
+                        pattern = os.path.join(os.path.abspath(
+                            os.path.dirname(gitignore)), line)
+                        ignored.add(pattern)
+
+        # Now let's look at all the templates and note macro calls.
+
+        # A regex to match macro calls from the template context. Note that it is
+        # imprecise, so the resulting analysis could generate false negatives.
+        call = re.compile(r'/\*[-\?].*?\bmacros\.([a-zA-Z][a-zA-Z0-9_]*)\b')
+
+        used = set()
+        for root, _, files in os.walk(os.path.abspath(
+                os.path.join(MY_DIR, '..'))):
+            for f in (os.path.join(root, f) for f in files):
+                for pattern in ignored:
+                    try:
+                        if fnmatch.fnmatchcase(f, pattern):
+                            break
+                    except Exception:
+                        # Suppress any errors resulting from invalid lines in
+                        # .gitignore.
+                        pass
+                else:
+                    # This file did not match any of the ignore patterns; scan
+                    # it for macro calls.
+
+                    with open(f) as input:
+                        source = input.read()
+                    for m in call.finditer(source):
+                        used.add(m.group(1))
+
+        unused = macros - used
+
+        self.assertLen(unused, 0)
 
 if __name__ == '__main__':
     unittest.main()
