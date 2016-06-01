d123d36e83ee7cbe49d209f0b05725fb652126cc
Matthew Fernandez <matthew.fernandez@nicta.com.au>
2016-06-02 08:54:17 +1000

    libsel4camkes: Use the COLD_PATH macro on the error path instead.
    This commit cherry-picks ec949d550c7b0e509b562e3a92142d25192bb524 from
    ssh://github.inside.nicta.com.au/mfernandez/project.

--- libsel4camkes/include/camkes/error.h
+++ libsel4camkes/include/camkes/error.h
@@ -231,8 +231,7 @@ camkes_error_action_t camkes_error(camkes_error_t *e) COLD;
      *    statement).
      */
     #define ERR(handler, edata, action) ({ \
-        /* Tell the compiler to deprioritise this code path. */ \
-        JOIN(camkes_error_path_, __COUNTER__): COLD UNUSED; \
+            COLD_PATH(); \
             camkes_error_t _e = edata; \
             _e.filename = __FILE__; \
             _e.lineno = __LINE__; \
