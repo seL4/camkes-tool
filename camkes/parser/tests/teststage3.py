b4bc333e0415b2cd951ce7132b6be556a0decfcd
Matthew Fernandez <matthew.fernandez@nicta.com.au>
2016-06-08 15:22:47 +1000

    Add a test for the previous fixed bug.
    This commit cherry-picks ecb8954c3f90c387312aa77f0f07c4688d609503 from
    ssh://github.inside.nicta.com.au/mfernandez/project.

--- camkes/parser/tests/teststage3.py
+++ camkes/parser/tests/teststage3.py
@@ -21,7 +21,8 @@ ME = os.path.abspath(__file__)
 # Make CAmkES importable
 sys.path.append(os.path.join(os.path.dirname(ME), '../../..'))
 
-from camkes.ast import Component, Configuration, LiftedAST, Procedure, Setting
+from camkes.ast import Component, Configuration, Include, LiftedAST, \
+    Procedure, Setting
 from camkes.internal.tests.utils import CAmkESTest, cpp_available, \
     plyplus_introspectible
 from camkes.parser.stage0 import CPP, Reader
@@ -1081,6 +1082,42 @@ class TestStage3(CAmkESTest):
         '''
         content, _ = self.parser.parse_string(';;;')
         self.assertLen(content.children, 0)
+
+    def test_c_include(self):
+        '''
+        Test we can parse a relative C include.
+        '''
+        content, _ = self.parser.parse_string(
+            'procedure P { include "hello.h"; }')
+
+        self.assertLen(content.children, 1)
+        P = content.children[0]
+        self.assertIsInstance(P, Procedure)
+
+        self.assertLen(P.children, 1)
+        include = P.children[0]
+        self.assertIsInstance(include, Include)
+
+        self.assertTrue(include.relative)
+        self.assertEqual(include.source, 'hello.h')
+
+    def test_c_include2(self):
+        '''
+        Test we can parse a relative C include that relies on multi strings.
+        '''
+        content, _ = self.parser.parse_string(
+            'procedure P { include "hello" "world"; }')
+
+        self.assertLen(content.children, 1)
+        P = content.children[0]
+        self.assertIsInstance(P, Procedure)
+
+        self.assertLen(P.children, 1)
+        include = P.children[0]
+        self.assertIsInstance(include, Include)
+
+        self.assertTrue(include.relative)
+        self.assertEqual(include.source, 'helloworld')
 
 if __name__ == '__main__':
     unittest.main()
