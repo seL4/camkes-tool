8e5b6a85174bcdc56817cefcd6f6d67f4dbc9da1
Matthew Fernandez <matthew.fernandez@nicta.com.au>
2016-06-08 14:27:11 +1000

    Test string concatenation across newlines.
    This commit cherry-picks bff4e56b94d1eeb6259fcf1fdf263ca92ca7a7da from
    ssh://github.inside.nicta.com.au/mfernandez/project.

--- camkes/parser/tests/teststage3.py
+++ camkes/parser/tests/teststage3.py
@@ -1051,5 +1051,29 @@ class TestStage3(CAmkESTest):
 
         self.assertEqual(foobar.value, 'helloworld')
 
+    def test_string_concat_line_split(self):
+        '''
+        Test that C-style string concatenation works across line breaks.
+        '''
+
+        content, _ = self.parser.parse_string(
+            'configuration {\n'
+            '  foo.bar = "hello" \n'
+            '"world";\n'
+            '}')
+
+        self.assertLen(content.children, 1)
+        conf = content.children[0]
+        self.assertIsInstance(conf, Configuration)
+
+        self.assertLen(conf.settings, 1)
+        foobar = conf.settings[0]
+        self.assertIsInstance(foobar, Setting)
+
+        self.assertEqual(foobar.instance, 'foo')
+        self.assertEqual(foobar.attribute, 'bar')
+
+        self.assertEqual(foobar.value, 'helloworld')
+
 if __name__ == '__main__':
     unittest.main()
