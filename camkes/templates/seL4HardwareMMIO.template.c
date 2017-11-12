b6f2fedd8fa81665d4aa1780fdc10ea48c470131
Adrian Danis <Adrian.Danis@data61.csiro.au>
2017-11-13 10:08:10 +1100

    Refactored hardware dataport frame allocation
    When using register_shared_variable only one caller can pass in already allocated frames.
    This refactors the frame allocation so that in an N-to-1 scenario only the first template
    passes in the frames to register_shared_variable, and the other templates receive the
    frame caps through the per template data stash.

--- camkes/templates/seL4HardwareMMIO.template.c
+++ camkes/templates/seL4HardwareMMIO.template.c
@@ -87,22 +87,36 @@ void * /*? me.interface.name ?*/_translate_paddr(
     return NULL;
 }
 
-/*- set frame_caps = c_symbol('frame_caps') -*/
-/*- set frames = [] -*/
-/*# Allocate frame objects to back the hardware dataport #*/
-static const seL4_CPtr /*? frame_caps ?*/[] = {
+/*- set frame_caps_symbol = c_symbol('frame_caps') -*/
+/*- set frame_caps = pop('frame_caps') -*/
+/*- set frame_objs = none -*/
+/*- if frame_caps is none -*/
+    /*- set frame_caps = [] -*/
+    /*- set frame_objs = [] -*/
     /*- set n_frames = (size + macros.PAGE_SIZE - 1) // macros.PAGE_SIZE -*/
     /*- for i in range(n_frames) -*/
         /*- set name = "frame_%s_%d" % (me.instance.name, i) -*/
         /*- set offset = macros.PAGE_SIZE * i -*/
         /*- set frame_obj = alloc_obj(name, seL4_FrameObject, paddr=(paddr + offset)) -*/
-        /*- do frames.append(frame_obj) -*/
+        /*- do frame_objs.append(frame_obj) -*/
         /*- set frame_cap = alloc_cap(name, frame_obj) -*/
-        /*? frame_cap ?*/,
+        /*- do frame_caps.append(frame_cap) -*/
     /*- endfor -*/
+/*- endif -*/
+/*# Always restash as 'pop' removes them #*/
+/*- do stash('frame_caps', frame_caps) -*/
+
+/*# Allocate frame objects to back the hardware dataport #*/
+static const seL4_CPtr /*? frame_caps_symbol ?*/[] = {
+        /*- for cap in frame_caps -*/
+            /*? cap ?*/,
+        /*- endfor -*/
 };
 
-/*- do register_shared_variable('%s_data' % me.parent.name, 'from_%d_%s_data' % (index, me.interface.name), 'RW', paddr, frames=frames, cached_hw=cached) -*/
+/*# We only pull frame_caps from the stash. This is because only one caller of register_shared_variable
+    should pass a frames parameter. By not stashing the frame_objs we ensure that only the original
+    creator passed the frames, and everyone else will still have a None here #*/
+/*- do register_shared_variable('%s_data' % me.parent.name, 'from_%d_%s_data' % (index, me.interface.name), 'RW', paddr, frames=frame_objs, cached_hw=cached) -*/
 
 /*- if options.architecture in ('aarch32', 'arm_hyp') -*/
     static int sel4_cache_op(seL4_CPtr frame_cap, seL4_Word start, seL4_Word end, dma_cache_op_t cache_op) {
@@ -134,7 +148,7 @@ int /*? me.interface.name ?*/_flush_cache(size_t start_offset UNUSED, size_t siz
 
     while (current_offset < end_offset) {
         size_t frame_top = MIN(ROUND_UP(current_offset + 1, /*? macros.PAGE_SIZE ?*/), end_offset);
-        seL4_CPtr frame_cap = /*? frame_caps ?*/[current_offset / /*? macros.PAGE_SIZE ?*/];
+        seL4_CPtr frame_cap = /*? frame_caps_symbol ?*/[current_offset / /*? macros.PAGE_SIZE ?*/];
         size_t frame_start_offset = current_offset % /*? macros.PAGE_SIZE ?*/;
         size_t frame_end_offset = ((frame_top - 1) % /*? macros.PAGE_SIZE ?*/) + 1;
         int error = sel4_cache_op(frame_cap, frame_start_offset,  frame_end_offset, cache_op);
