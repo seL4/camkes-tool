135a5447fb050de64c4b433d4ffc2de124fc9b13
Kofi Doku Atuah <Kofidoku.atuah@data61.csiro.au>
2018-08-31 15:36:30 +1000

    Parser: Exceptions: Add exceptions specific the DTB binding syntax
    The reason why these are necessary is that the test framework (testexamples.py)
    checks for failures in the "bad" tests by looking for categories of exceptions.
    
    In order to distinguish the DTB Binding exceptions from the other exceptions and
    keep the tests clean, it was necessary to add a new set of exceptions which all derive
    from a common base class.

--- camkes/parser/exception.py
+++ camkes/parser/exception.py
@@ -34,3 +34,33 @@ class ParseError(CAmkESError):
         msg = self._format_message(six.text_type(content), filename, lineno,
             min_col, max_col)
         super(ParseError, self).__init__(msg)
+
+class OutcallError(ParseError):
+    def __init__(self, content, location=None, filename=None, lineno=None):
+        # Allow other embedded syntaxes to have more full featured exceptions.
+        super(OutcallError, self).__init__(content, location, filename, lineno)
+
+class DtbBindingError(OutcallError):
+    def __init__(self, content):
+        # But I personally only care about transmitting a string.
+        super(DtbBindingError, self).__init__(content)
+
+class DtbBindingQueryFormatError(DtbBindingError):
+    def __init__(self, content):
+        super(DtbBindingQueryFormatError, self).__init__(content)
+
+class DtbBindingNodeLookupError(DtbBindingError):
+    def __init__(self, content):
+        super(DtbBindingNodeLookupError, self).__init__(content)
+
+class DtbBindingSyntaxError(DtbBindingError):
+    def __init__(self, content):
+        super(DtbBindingSyntaxError, self).__init__(content)
+
+class DtbBindingTypeError(TypeError, DtbBindingError):
+    def __init__(self, content):
+        super(DtbBindingTypeError, self).__init__(content)
+
+class DtbBindingNotImplementedError(NotImplementedError, DtbBindingError):
+    def __init__(self, content):
+        super(DtbBindingNotImplementedError, self).__init__(content)
