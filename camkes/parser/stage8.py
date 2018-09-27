seL4/camkes-tool

commit eaf89ecd6ab2a16c2d5d63405e32ad04f9458f5f

parser: allow for dict lookups in attribute references
This commit adds the ability to look up values in attribute references
that are dictionaries.

The syntax is the same as python dict lookups and is as follows:

s.bah <- y.attribute['key']['anotherkey']

camkes/parser/stage8.py
@@ -106,8 +106,13 @@ def resolve(ast_lifted):
             if not sub_resolve(referents[0], depth +  [setting.value.reference]):
                 setting.value = None
                 return False
-
-        setting.value = referents[0].value
+        elif setting.value.dict_lookup and isinstance(referents[0].value, dict):
+            value = referents[0].value
+            for key in setting.value.dict_lookup.lookup:
+                value = value[key]
+            setting.value = value
+        else:
+            setting.value = referents[0].value
         return True
 
     # Iterate through each setting we need to resolve

camkes/ast/__init__.py
@@ -21,7 +21,7 @@ from .liftedast import LiftedAST
 from .location import SourceLocation
 from .objects import Assembly, Attribute, AttributeReference, Component, \
     Composition, Configuration, Connection, ConnectionEnd, Connector, \
-    Consumes, Dataport, Emits, Export, Group, Include, Instance, Interface, \
+    Consumes, Dataport, DictLookup, Emits, Export, Group, Include, Instance, Interface, \
     Method, Mutex, Parameter, Procedure, Provides, Reference, Semaphore, \
     BinarySemaphore, Setting, Uses, Struct
 from .traversal import SimpleTraversalContext, TraversalAction, \

camkes/ast/objects.py
@@ -1886,10 +1886,11 @@ class Export(ASTObject):
         self._destination = value
 
 class AttributeReference(ASTObject):
-    def __init__(self, reference, location=None):
+    def __init__(self, reference, dict_lookup, location=None):
         assert isinstance(reference, six.string_types)
         super(AttributeReference, self).__init__(location)
         self._reference = reference
+        self._dict_lookup = dict_lookup
 
     @property
     def reference(self):
@@ -1901,3 +1902,13 @@ class AttributeReference(ASTObject):
             raise TypeError('you cannot set the \'reference\' field of a '
                 'frozen object')
         self._reference = value
+
+    @property
+    def dict_lookup(self):
+        return self._dict_lookup
+
+
+class DictLookup(ASTObject):
+    def __init__(self, lookup, location):
+        super(DictLookup, self).__init__(location)
+        self.lookup = lookup

camkes/parser/camkes.g
@@ -135,7 +135,7 @@ export: EXPORT reference '->' reference ';';
 configuration_decl: CONFIGURATION id? configuration_defn;
 configuration_defn: '\{' setting* '\}';
 setting: id '\.' id (('=' item) | ('<-' attribute_reference)) ';';
-attribute_reference: id ('\.' id)*;
+attribute_reference: id ('\.' id)* (dict_lookup)?;
 
 connector_decl: CONNECTOR id? connector_defn;
 connector_defn: '\{' FROM hardware_bare? connector_end_type connector_properties ';' TO hardware_bare? connector_end_type connector_properties ';'
@@ -223,6 +223,7 @@ quoted_string: '"[^"]*"'; // "
 angle_string: '<[^>]*>';
 list: '\[' (item (',' item)* ','?)? '\]';
 dict: '\{' (key ':' item (',' key ':' item)* ','?)? '\}';
+dict_lookup: ('\[' key '\]')+;
 @key: numeric_expr | multi_string;
 @item: numeric_expr | multi_string | list | dict;
 boolean_literal: TRUE1 | TRUE2 | FALSE1 | FALSE2;

camkes/parser/stage3.py
@@ -26,7 +26,7 @@ from camkes.internal.seven import cmp, filter, map, zip
 
 from camkes.ast import Assembly, Attribute, AttributeReference, Component, \
     Composition, Configuration, Connection, ConnectionEnd, Connector, \
-    Consumes, Dataport, Emits, Export, Group, Include, Instance, Interface, \
+    Consumes, Dataport, DictLookup, Emits, Export, Group, Include, Instance, Interface, \
     LiftedAST, Method, Mutex, normalise_type, Parameter, Procedure, Provides, \
     Reference, Semaphore, BinarySemaphore, Setting, SourceLocation, Uses, Struct
 from .base import Parser
@@ -173,8 +173,13 @@ def _lift_attribute(location, attribute_param, default=None):
         default = default[1:-1] # Strip quotes
     return Attribute(attribute_param.type, attribute_param.name, attribute_param.array, default, location)
 
-def _lift_attribute_reference(location, *ids):
-    return AttributeReference('.'.join(ids), location)
+def _lift_attribute_reference(location, *args):
+    dict_lookup = None
+    args = list(args)
+    if len(args) and isinstance(args[-1], DictLookup):
+        dict_lookup = args.pop()
+
+    return AttributeReference('.'.join(args), dict_lookup, location)
 
 def _lift_boolean_literal(location, text):
     if text in ('True', 'true'):
@@ -664,6 +669,12 @@ def _lift_uses(location, *args):
         type, name = args
     return Uses(Reference(type.name, Procedure, type.location), name, optional, location)
 
+def _lift_dict_lookup(location, *args):
+    new_args = []
+    for arg in args:
+        new_args.append(strip_quotes(arg))
+    return DictLookup(new_args, location)
+
 def _collapse(location, content):
     return content
 
@@ -700,6 +711,7 @@ LIFT = {
     'dataport':_lift_dataport,
     'dataport_type':_lift_dataport_type,
     'dict':_lift_dict,
+    'dict_lookup' : _lift_dict_lookup,
     'direction':_collapse,
     'emits':_lift_emits,
     'export':_lift_export,

commit 4dc488e7c8e4c2475f5d3613f15b38b7f070c120

parser: fix bug introduced in eaf89ecd6ab

camkes/parser/stage8.py
@@ -106,6 +106,7 @@ def resolve(ast_lifted):
             if not sub_resolve(referents[0], depth +  [setting.value.reference]):
                 setting.value = None
                 return False
+            setting.value = referents[0].value
         elif setting.value.dict_lookup and isinstance(referents[0].value, dict):
             value = referents[0].value
             for key in setting.value.dict_lookup.lookup: