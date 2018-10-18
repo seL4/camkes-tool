038cb7a7860c4da05ae35a88d064778db9a38af6
Adam Felizzi <Adam.Felizzi@data61.csiro.au>
2018-10-18 12:00:59 +1100

    templates: CakeML derive_eval_thm in rpc template
    Our rpc template requires the derive_eval_thm function which is no
    longer availabe after updating our CakeML toolchain. The
    derive_eval_thm function has been ported to the rpc-connector
    template.

--- camkes/templates/rpc-connector.c
+++ camkes/templates/rpc-connector.c
@@ -91,6 +91,25 @@
             /*- endif -*/
         /*- endif -*/
     /*- elif namespace.language == 'cakeml' -*/
+        (* TODO: Move the derive_eval_thm_bytes function to a common library location for other cakeml src to use *)
+        fun derive_eval_thm_bytes for_eval v_name e len = let
+            val th = get_ml_prog_state () |> get_thm
+            val th = MATCH_MP ml_progTheory.ML_code_Dlet_var th
+                   |> REWRITE_RULE [ml_progTheory.ML_code_env_def]
+            val th = th |> CONV_RULE(RESORT_FORALL_CONV(sort_vars["e","s3"]))
+                      |> SPEC e
+            val st = th |> SPEC_ALL |> concl |> dest_imp |> #1 |> strip_comb |> #2 |> el 1
+            val new_st = ``^st with refs := ^st.refs ++ [W8array (REPLICATE ^(numSyntax.term_of_int len) 0w)]``
+            val goal = th |> SPEC new_st |> SPEC_ALL |> concl |> dest_imp |> fst
+            val lemma = goal |> (EVAL THENC SIMP_CONV(srw_ss())[semanticPrimitivesTheory.state_component_equality])
+            val v_thm = prove(mk_imp(lemma |> concl |> rand, goal),
+            rpt strip_tac \\ rveq \\ match_mp_tac(#2(EQ_IMP_RULE lemma))
+            \\ asm_simp_tac bool_ss [])
+            |> GEN_ALL |> SIMP_RULE std_ss [] |> SPEC_ALL;
+            val v_tm = v_thm |> concl |> strip_comb |> #2 |> last
+            val v_def = define_abbrev false v_name v_tm
+            in v_thm |> REWRITE_RULE [GSYM v_def] end
+
         /*- if buffer is not none -*/
             /*? raise(TemplateError('CakeML connector only supports using the IPC buffer')) ?*/
         /*- endif -*/
@@ -102,6 +121,7 @@
         val ipcbuf_e = ``(App Aw8alloc [Lit (IntLit /*? bsize ?*/); Lit (Word8 0w)])``
         /*- set namespace.cakeml_reserved_buf = 17 -*/
         val eval_thm = derive_eval_thm false "ipcbuf_loc" ipcbuf_e;
+        val eval_thm = derive_eval_thm_bytes false "ipcbuf_loc" ipcbuf_e /*? bsize ?*/;
         val _ = ml_prog_update (add_Dlet eval_thm "ipcbuf" []);
     /*- endif -*/
     /*- set namespace.send_buffer = base -*/
