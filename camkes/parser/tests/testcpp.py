265edaef2d357c006d3cef9ffedc892888388f13
Kent McLeod <Kent.Mcleod@data61.csiro.au>
2019-04-01 16:34:31 +1100

    parser: Remove test_toolprefix
    We no longer use a toolprefix with cpp.

--- camkes/parser/tests/testcpp.py
+++ camkes/parser/tests/testcpp.py
@@ -87,60 +87,6 @@ class TestCPP(CAmkESTest):
         self.assertIn(child, read)
 
     @unittest.skipIf(not cpp_available(), 'CPP not found')
-    def test_toolprefix(self):
-        # Find CPP.
-        cpp = subprocess.check_output(['which', 'cpp'],
-            universal_newlines=True).strip()
-
-        # Create a fake CPP.
-        tmp = self.mkdtemp()
-        toolprefix = 'my-toolprefix-'
-        fake_cpp = os.path.join(tmp, '%scpp' % toolprefix)
-        with open(fake_cpp, 'wt') as f:
-            f.write('#!/bin/bash\n%s "$@"\n' % cpp)
-        os.chmod(fake_cpp, stat.S_IRWXU)
-
-        # Create some fake input.
-        parent = self.mkstemp()
-        child = self.mkstemp()
-        with open(child, 'wt') as f:
-            f.write('component foo {}\n')
-        with open(parent, 'wt') as f:
-            f.write('#include "%s"\n' % child)
-
-        # Change the value of $PATH to affect the exec inside parse_string,
-        # such that the original CPP won't be found. We want to do this to
-        # ensure the test doesn't accidentally succeed because `toolprefix` is
-        # ignored and we happen to exec the original CPP directly.
-        old_path = os.environ['PATH']
-        os.environ['PATH'] = tmp
-
-        try:
-            # Confirm CPP is not directly accessible.
-            with self.assertRaises(OSError):
-                with open(os.devnull, 'wt') as f:
-                    subprocess.check_call(['cpp', '--version'], stdout=f,
-                        stderr=f)
-
-            # Make sure the stage 0 parser agrees with us.
-            with self.assertRaises(OSError):
-                p = CPP()
-                p.parse_string('')
-
-            # Now confirm it can see and use our fake CPP.
-            parser = CPP(toolprefix)
-
-            content, read = parser.parse_file(parent)
-
-            self.assertIn('component', content)
-            self.assertIn(parent, read)
-            self.assertIn(child, read)
-
-        finally:
-            # Reset the environment.
-            os.environ['PATH'] = old_path
-
-    @unittest.skipIf(not cpp_available(), 'CPP not found')
     def test_flags(self):
         content, _ = self.reader.parse_string('HELLO')
         self.assertNotIn('world', content)
