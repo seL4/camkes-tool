1fc064d586cb89cef9ca700c6e47c5c245ffeaa1
Kent McLeod <Kent.Mcleod@data61.csiro.au>
2019-04-11 10:53:30 +1000

    libsel4camkes: Add riscv entry and fault code
    This is needed for running standard camkes components on RISCV targets.

---
+++ libsel4camkes/src/arch/riscv/unknown_syscall_fault.c
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2019, Data61
+ * Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+ * ABN 41 687 119 230.
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(DATA61_BSD)
+ */
+
+#include "../../arch_fault.h"
+#include <assert.h>
+#include <sel4/sel4.h>
+#include <stddef.h>
+#include <stdint.h>
+
+void show_unknown_syscall_fault(seL4_CPtr thread_id, const char *name)
+{
+    assert(name != NULL);
+
+    uintptr_t pc = seL4_GetMR(0);
+    uintptr_t sp = seL4_GetMR(1);
+    uintptr_t lr = seL4_GetMR(2);
+    uintptr_t a0 = seL4_GetMR(3);
+    uintptr_t a1 = seL4_GetMR(4);
+    uintptr_t a2 = seL4_GetMR(5);
+    uintptr_t a3 = seL4_GetMR(6);
+    uintptr_t a4 = seL4_GetMR(7);
+    uintptr_t a5 = seL4_GetMR(8);
+    uintptr_t a6 = seL4_GetMR(9);
+    int syscall = seL4_GetMR(10);
+    SHOW("unknown syscall (%d) from %s (ID 0x%x), pc = %p\n"
+         "     a0 = %p\n"
+         "     a1 = %p\n"
+         "     a2 = %p\n"
+         "     a3 = %p\n"
+         "     a4 = %p\n"
+         "     a5 = %p\n"
+         "     a6 = %p\n"
+         "     sp = %p\n"
+         "     lr = %p\n",
+         syscall, name, thread_id, (void *)pc, (void *)a0, (void *)a1, (void *)a2,
+         (void *)a3, (void *)a4, (void *)a5, (void *)a6, (void *)sp,
+         (void *)lr);
+}
