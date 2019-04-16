8ecf3652103f7c94b9c2a55ecfca48e340dab6da
Damon Lee <Damon.Lee@data61.csiro.au>
2019-04-16 13:36:32 +1000

    libsel4camkes: Add ioport_region struct, constants
    The new struct is used for describing an IO port region allocated by
    CAmkES. The constants is for providing meaning to the IO port byte
    sizes and not leaving them as magic numbers, i.e. 1, 2, and 4.

--- libsel4camkes/include/camkes/io.h
+++ libsel4camkes/include/camkes/io.h
@@ -15,6 +15,13 @@
 
 #include <platsupport/io.h>
 #include <stdint.h>
+#include <sel4/sel4.h>
+#include <camkes/error.h>
+#include <utils/attribute.h>
+
+#define IOSIZE_8 1
+#define IOSIZE_16 2
+#define IOSIZE_32 4
 
 /* Initialise an IO mapper for use with libplatsupport. Returns 0 on success.
  */
@@ -34,5 +41,25 @@ int camkes_io_ops(ps_io_ops_t *ops);
  * on success.
  */
 int camkes_ps_malloc_ops(ps_malloc_ops_t *ops);
+
+/*
+ * This struct describes an IO port region that is allocated by CAmkES.
+ *
+ * Instances of this struct is intended to be located inside a section in an ELF file.
+ */
+struct ioport_region {
+    uint16_t start;
+    uint16_t end;
+    seL4_CPtr cap;
+    camkes_error_handler_t error_handler;
+    char **interface_name;
+};
+typedef struct ioport_region ioport_region_t;
+
+/* Force the _ioport_region section to be created even if no modules are defined. */
+static USED SECTION("_ioport_regions") struct {} dummy_ioport_region;
+/* Definitions so that we can find the exposed IO port regions */
+extern ioport_region_t *__start__ioport_regions[];
+extern ioport_region_t *__stop__ioport_regions[];
 
 #endif
