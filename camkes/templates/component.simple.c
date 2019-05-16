927ac174ad4f8146948688c6151ba4ebb7faf06c
Anna Lyons <Anna.Lyons@data61.csiro.au>
2019-05-16 10:40:56 +1000

    camkes/templates: update for capdl api change
    Use CDL_FrameFill_BootInfo constant instead of arbitrary "bootinfo"
    string for specifying frame fill type.

--- camkes/templates/component.simple.c
+++ camkes/templates/component.simple.c
@@ -47,7 +47,7 @@
     struct {
         char content[PAGE_SIZE_4K];
     } /*? symbol ?*/ ALIGN(PAGE_SIZE_4K) SECTION("align_12bit");
-    /*- do register_fill_frame(symbol, 'bootinfo 0 %s' % name) -*/
+    /*- do register_fill_frame(symbol, 'CDL_FrameFill_BootInfo 0 %s' % name) -*/
     /*- do extrabi_list.append((name, symbol)) -*/
 /*- endfor -*/
 
