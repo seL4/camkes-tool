3685f30108d15eb77a36a4040899f829791eff2b
Kent McLeod <Kent.Mcleod@data61.csiro.au>
2019-11-05 14:31:06 +1100

    camkes-gen.cmake: Remove capdl MAX_IRQS field
    This field is no longer needed.

--- camkes/templates/camkes-gen.cmake
+++ camkes/templates/camkes-gen.cmake
@@ -525,7 +525,6 @@ add_dependencies(camkes_capdl_target object_sizes ${capdl_elf_targets})
 
 # Invoke the parse-capDL tool to turn the CDL spec into a C spec
 CapDLToolCFileGen(capdl_c_spec_target capdl_spec.c ${CAmkESCapDLStaticAlloc} "$<TARGET_PROPERTY:object_sizes,FILE_PATH>" "${CAMKES_CDL_TARGET}" "${CAPDL_TOOL_BINARY}"
-    MAX_IRQS ${CapDLLoaderMaxIRQs}
     DEPENDS camkes_capdl_target install_capdl_tool "${CAPDL_TOOL_BINARY}")
 
 # Ask the CapDL tool to generate an image with our given copied/mangled instances
