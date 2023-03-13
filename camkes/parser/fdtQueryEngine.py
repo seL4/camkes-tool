c271043ad8b9f7a5d3f4783d669dfdc39fa70cf1
Axel Heider <axel.heider@hensoldt.net>
2023-03-13 18:27:42 +0100

    parser: remove helper variable
    Also drop the comments, because one is misleading and the other one just
    states what is obvious.
    
    Signed-off-by: Axel Heider <axel.heider@hensoldt.net>

--- camkes/parser/fdtQueryEngine.py
+++ camkes/parser/fdtQueryEngine.py
@@ -598,8 +598,6 @@ class DtbMatchQuery(Query):
 
     def resolve(self, args):
         result = self.engine.query(args)
-
-        resolved = {}
         if not len(result):
             raise ParseError("DTB query has no results.")
 
@@ -611,11 +609,11 @@ class DtbMatchQuery(Query):
                 node = entry[0]
                 node_resolved = self.resolve_fdt_node(node)
                 query_results.append(node_resolved)
-        # place the results under the 'dtb' key
-        resolved['query'] = query_results
-        # inject the size of the dtb into into the dictionary
-        resolved['dtb-size'] = [self.dtb_file_size]
-        return resolved
+
+        return {
+            'query': query_results,
+            'dtb-size': [self.dtb_file_size]
+        }
 
     @staticmethod
     def get_parser():
